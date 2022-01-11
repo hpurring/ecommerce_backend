9c18c0de9416079c390d37e85721c9f53b057221
hpurring <hilarypurrington@gmail.com>
2022-01-10 23:10:27 -0500

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -160,6 +160,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    })
+    .then(dbProductData => {
+      if(!dbProductData) {
+        res.status(404).json({message: 'No product found with this id'})
+        return;
+      }
+      res.status(200).json(dbProductData)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err)
+    })
 });
 
 module.exports = router;
